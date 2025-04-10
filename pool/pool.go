4a4170c673d6ca6adc0a59efc493816ac631305d
Lo <wenshi.fu@foxmail.com>
2025-04-11 00:04:03 +0800

    feat: suppot PoolWithResetFn

--- pool/pool.go
+++ pool/pool.go
@@ -5,12 +5,12 @@ import "sync"
 // A Pool is a type-safe wrapper around a sync.Pool.
 // Support generic
 type Pool[T any] struct {
-	pool sync.Pool
+	_pool sync.Pool
 }
 
 func New[T any](fn func() T) *Pool[T] {
 	return &Pool[T]{
-		pool: sync.Pool{
+		_pool: sync.Pool{
 			New: func() any {
 				return fn()
 			},
@@ -18,9 +18,9 @@ func New[T any](fn func() T) *Pool[T] {
 	}
 }
 func (p *Pool[T]) Get() T {
-	return p.pool.Get().(T)
+	return p._pool.Get().(T)
 }
 
 func (p *Pool[T]) Put(v T) {
-	p.pool.Put(v)
+	p._pool.Put(v)
 }
---
+++ pool/pool_with_reset.go
@@ -0,0 +1,30 @@
+package pool
+
+import "sync"
+
+type PoolWithResetFn[T any] struct {
+	_pool      sync.Pool
+	_resetFunc func(T)
+}
+
+func NewPoolWithResetFn[T any](newFn func() T, resetFn func(T)) PoolWithResetFn[T] {
+	return PoolWithResetFn[T]{
+		_pool: sync.Pool{
+			New: func() any {
+				return newFn
+			},
+		},
+		_resetFunc: resetFn,
+	}
+}
+
+func (p *PoolWithResetFn[T]) Get() T {
+	return p._pool.Get().(T)
+}
+
+func (p *PoolWithResetFn[T]) Put(v T) {
+	if p._resetFunc != nil {
+		p._resetFunc(v)
+	}
+	p._pool.Put(v)
+}
